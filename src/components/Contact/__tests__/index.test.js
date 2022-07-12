e752c3ec7b507a68db1766553a85e30cd08a5d95
Candice Hall <hall.candice@outlook.com>
2022-07-12 12:50:43 -0400

    wrote tests for contact component

---
+++ src/components/Contact/__tests__/index.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Contact from '..';
+import ContactForm from '..';
+
+afterEach(cleanup);
+
+describe('Contact component', () => {
+    // baseline test
+    it('renders', () => {
+        render(<ContactForm />);
+    });
+
+    // snapshot test
+    it('matches snapshot', () => {
+        const { asFragment } = render(<ContactForm
+        />);
+        // assert value comparison
+        expect(asFragment()).toMatchSnapshot();
+    });
+})
+
+it('renders', () => {
+    const { getByTestId } = render(<ContactForm />)
+    expect(getByTestId('h1tag')).toHaveTextContent('Contact me')
+  })
+   
+  it('renders', () => {
+    const { getByTestId } = render(<ContactForm />)
+    expect(getByTestId('button')).toHaveTextContent('Submit')
+  })
+
